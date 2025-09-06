1a2f22bfe8b0bb5fb369e50a06d9271438160350
laclustr <28vance.grant@rrcs.org>
2025-09-06 14:41:31 -0400

    initialized xcode project

---
+++ SME/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SME
+//
+//  Created by Grant Vance on 9/6/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
